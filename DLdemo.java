d929d112d323565ef84135b7c2093434475a19fa
VandanaVipparthi <victoryvandana82@gmail.com>
2022-06-27 21:14:16 +0530

    demo for doubly ended list

---
+++ DLdemo.java
@@ -0,0 +1,15 @@
+package linkedlists;
+
+public class DLdemo {
+
+	public static void main(String[] args) {
+		DoublyEndedList dl=new DoublyEndedList();
+		dl.insertAtTail(10);
+		dl.insertAtTail(15);
+		dl.insertAtTail(20);
+
+		System.out.println(dl);
+
+	}
+
+}
